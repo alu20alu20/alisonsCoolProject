df8c43afddbd00fbcf30401232ae1696e7a09fb6
alu20alu20 <student10@js-c02mxf7ug083.punahou.edu>
2015-07-16 13:34:55 -1000

    I think thiss fixes it?

--- MusicApp.js
+++ MusicApp.js
@@ -1,11 +1,11 @@
-$('#deathcab').click(function(){
-  $('#dcsound').get(0).play();
+$('#deathcab').click(function() {
+    $('#dcsound').get(0).paused ? $('#dcsound').get(0).play() : $('#dcsound').get(0).pause();
 });
 
-$('#sleeping').click(function(){
-  $('#ssound').get(0).play();
+$('#sleeping').click(function() {
+    $('#ssound').get(0).paused ? $('#ssound').get(0).play() : $('#ssound').get(0).pause();
 });
 
-$('#am-i-wrong').click(function(){
-  $('#aiwsound').get(0).play();
+$('#am-i-wrong').click(function() {
+    $('#aiwsound').get(0).paused ? $('#aiwsound').get(0).play() : $('#aiwsound').get(0).pause();
 });
