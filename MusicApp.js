8ca8fc4c0063075caf058dc1846d23732d5ecf05
alu20alu20 <student10@js-c02mxf7ug083.punahou.edu>
2015-07-17 12:54:50 -1000

    erugfkdjnbm,

--- MusicApp.js
+++ MusicApp.js
@@ -9,3 +9,7 @@ $('#sleeping').click(function() {
 $('#am-i-wrong').click(function() {
     $('#aiwsound').get(0).paused ? $('#aiwsound').get(0).play() : $('#aiwsound').get(0).pause();
 });
+
+$('#canwedance').click(function() {
+    $('#cwdsound').get(0).paused ? $('cwdsound').get(0).play() : $('#cwdsound').get(0).pause();
+});
